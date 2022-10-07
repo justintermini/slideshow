9b34f99cddd28c85ac6218a5f4a452d941178c2e
justintermini <45074191+justintermini@users.noreply.github.com>
2022-10-07 09:34:06 -0400

    Fixed fullscreen issue

--- slideshow.py
+++ slideshow.py
@@ -21,7 +21,7 @@ class gui:
     def pic(self):
         
         self.pic_list = []
-        path = r'/home/odroid/slideshow/imgs/*' # Set path to reflect folder where imgs live 
+        path = r'/home/odroid/imgs/*' # Set path to reflect folder where imgs live 
 
         for name in glob.glob(path):
             val = name
@@ -49,8 +49,14 @@ def close_escape(event=None):
         
 root = tk.Tk()
 root.config(cursor="none") # Hide the mouse cursor
+
+# Determine screen width and height
+width_value = root.winfo_screenwidth()
+height_value = root.winfo_screenheight()
+# use values to create a string to put window in full monitor dimensions
+root.geometry("%dx%d+0+0" % (width_value, height_value))
+
 root.attributes('-fullscreen', True) # Use fullscreen to remove bar at top of image
-root.state('-zoomed', True) # Use full available screen proportions
 root.bind("<Escape>", close_escape)  # Calls close_escape function to close program when wanted
 myprog = gui(root)
 root.mainloop()
